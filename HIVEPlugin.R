57e66129295388e51acf264d49e6d0458a2949c0
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-06-16 12:31:20 -0400

    Initial commit

---
+++ HIVEPlugin.R
@@ -0,0 +1,25 @@
+library(bipartite)
+library(HiveR)
+
+set.seed(31)
+
+input <- function(inputfile) {
+  mydata <<- read.csv(inputfile, row.names=1, header=TRUE);
+}
+
+run <- function() {
+   hive1 <- adj2HPD(as.matrix(mydata), desc = "Safariland data set from bipartite")
+   sumHPD(hive1)
+   hive2 <- mineHPD(hive1, option = "rad <- tot.edge.count")
+   sumHPD(hive2)
+   hive3 <- mineHPD(hive2, option = "axis <- source.man.sink")
+   sumHPD(hive3, chk.all = TRUE)
+   hive3$edges$weight <- sqrt(hive3$edges$weight)*0.5
+   hive3$nodes$size <- 0.5
+   plotHive(hive3)
+}
+
+output <- function(outputfile) {
+   system(paste("mv Rplots.pdf ", outputfile, sep=""))
+}
+
